1a2c8e3ede7f6b2a521ae44198ccaf41fecc1a79
Павел Ширяев <p.shiryaev@qpdev.ru>
2024-01-29 17:52:43 +0300

    fix: темная тема для чекбокса

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import type { Todo } from '@/types/Todo';
 import Button from '@/UI/Button.vue';
+import { inject } from 'vue';
+import { Theme } from '@/types/Theme.enum';
 
+const theme = inject<Theme>('theme');
 const { id, todo } = defineProps<Todo>();
 const completed = defineModel('completed');
 </script>
 
 <template>
     <div class="todo-item">
-        <input type="checkbox" v-model="completed">
+        <input type="checkbox" :class="[theme, 'checkbox']" v-model="completed">
         <div class="todo-text">{{ todo }}</div>
         <Button class="del-btn" @click="$emit('removeTodo', id)">X</Button>
     </div>
@@ -34,10 +37,15 @@ const completed = defineModel('completed');
     align-items: center;
     cursor: pointer;
 }
-input[type=checkbox] {
+.checkbox {
     width: 40px;
     height: 40px;
     cursor: pointer;
+}
+.dark {
+    accent-color: white;
+}
+.light {
     accent-color: darkslategray;
 }
 </style>
